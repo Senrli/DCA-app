4b8ff4cd6473d528e92b836ec7090491908a47f9
mckp0 <michaelchunkp@gmail.com>
2022-07-12 14:54:15 +0800

    task module test

--- src/client/teamsPocTab/TeamsPocTab.tsx
+++ src/client/teamsPocTab/TeamsPocTab.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import { Provider, Flex, Text, Button, Header } from '@fluentui/react-northstar';
+import { Provider, Flex, Text, Button, Header, Divider } from '@fluentui/react-northstar';
 import { useState, useEffect } from 'react';
 import { useTeams } from 'msteams-react-base-component';
 import { app, authentication } from '@microsoft/teams-js';
 import jwtDecode from 'jwt-decode';
+
+import * as microsoftTeams from "@microsoft/teams-js";
+// import * as constants from "./constants";
+// import { cardTemplates, appRoot } from "./dialogs/CardTemplates";
+// import { taskModuleLink } from "./utils/DeepLinks";
 
 /**
  * Implementation of the teams poc Tab content page
@@ -44,6 +49,30 @@ export const TeamsPocTab = () => {
     }
   }, [context]);
 
+  microsoftTeams.initialize();
+
+
+
+  function generateClaimForm(){
+    const taskModuleInfo = {
+      title:`hello`,
+      url: "https://www.youtube.com/watch?v=mB-1j6wqzYE&t=203s",
+      width:600,
+      height:350
+    };
+
+    microsoftTeams.tasks.startTask(taskModuleInfo);
+  }
+
+  // private appRoot(): string {
+  //   if (typeof window == "undefined") {
+  //     return "https://{{HOSTNAME}}";
+  //   } else {
+  //     return window.location.protocol + "//" + window.location.host;
+  //   }
+  // }
+  
+
   /**
    * The render() method to create the UI of the tab
    */
@@ -57,22 +86,21 @@ export const TeamsPocTab = () => {
         }}
       >
         <Flex.Item>
-          <Header content="This is your tab" />
+          <Header content="Discount Claims" />
         </Flex.Item>
         <Flex.Item>
           <div>
             <div>
               <Text content={`Hello ${name}`} />
             </div>
+            <div>
+              <Button content="Generate Discount Form" primary onClick={generateClaimForm}></Button>
+            </div>
             {error && (
               <div>
                 <Text content={`An SSO error occurred ${error}`} />
               </div>
             )}
-
-            <div>
-              <Button onClick={() => alert('It worked!')}>A sample button</Button>
-            </div>
           </div>
         </Flex.Item>
         <Flex.Item
@@ -80,7 +108,7 @@ export const TeamsPocTab = () => {
             padding: '.8rem 0 .8rem .5rem'
           }}
         >
-          <Text size="smaller" content="(C) Copyright Senrli" />
+          <Text size="smaller" content="(C) Copyright Beep|SUTD" />
         </Flex.Item>
       </Flex>
     </Provider>
