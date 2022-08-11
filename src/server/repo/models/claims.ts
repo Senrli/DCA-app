bb61026757d08a353557a16cee5c14faf098bf2b
Senrli <19980321lsr@gmail.com>
2022-08-11 11:00:24 +0800

    added claim schema

---
+++ src/server/repo/models/claims.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+export interface ClaimMemberData {
+  displayName: string;
+  userId: string;
+}
+
+export interface DocumentData {
+  displayName: string;
+  reference: string;
+  downloadLink: string;
+}
+
+export interface Claim {
+  caseId: string;
+  patientName: string;
+  requestor: ClaimMemberData;
+  creator: ClaimMemberData;
+  approver: ClaimMemberData;
+  documents: [DocumentData];
+}
+
+const ClaimMemberDataSchema = new mongoose.Schema(
+  {
+    displayName: String,
+    userId: String
+  },
+  { _id: false }
+);
+
+const DocumentDataSchema = new mongoose.Schema(
+  {
+    displayName: String,
+    reference: String,
+    downloadLink: String
+  },
+  { _id: false }
+);
+
+const ClaimSchema = new mongoose.Schema({
+  caseId: String,
+  patientName: String,
+  requestor: ClaimMemberDataSchema,
+  creator: ClaimMemberDataSchema,
+  approver: ClaimMemberDataSchema,
+  documents: [DocumentDataSchema]
+});
+
+export const ClaimModel = mongoose.model<Claim>('claim', ClaimSchema);
